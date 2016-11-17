1d42df520c90357bca4ed0ec382aea976026604f
Ethan Thacker <ethan.thacker@EthanThacker.local>
2016-11-17 14:47:43 -0600

    first commit

---
+++ src/js/contact.js
@@ -0,0 +1,3 @@
+submitContact = function () {
+
+};
---
+++ src/js/global.js
@@ -0,0 +1,15 @@
+var ethan = ethan || {};
+
+ethan.app = {
+    database:variable = null,
+    initPage : function() {
+       var config = {
+           apiKey: "AIzaSyDkEeXuSEzN3lbX92MRZcMm0KQVoCcNu38",
+           authDomain: "ethanthacker-2825c.firebaseapp.com",
+           databaseURL: "https://ethanthacker-2825c.firebaseio.com",
+           storageBucket: "ethanthacker-2825c.appspot.com",
+       };
+       firebase.initializeApp(config);
+       ethan.app.database = firebase.database();
+   }
+};
