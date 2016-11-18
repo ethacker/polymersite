b66a4d8e318e7d335e504d0b502e0ceb47b3468b
Ethan Thacker <ethan.thacker@EthanThacker.local>
2016-11-18 14:43:45 -0600

    contact db functions

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,3 +1,16 @@
-submitContact = function () {
+ethan = ethan || {};
 
+
+ethan.contact = {
+    submitContact : function() {
+        ethan.app.database.ref('contacts').push({
+            name: $('#name').val(),
+            email: $('#email').val(),
+            message: $('#message').val()
+        });
+
+        $('#name').val('');
+        $('#email').val('');
+        $('#message').val('');
+    }
 };
